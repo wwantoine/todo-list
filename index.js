0dfadabb0816b8380c40e64131c169899e360fdc
William <wwantoine@gmail.com>
2020-09-09 10:49:24 +0700

    update1

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ let todoList = []
 const addTodo = () => {
     let input = {content:document.getElementById("todoInput").value,isDone:false}
     todoList.push(input);
-    render();
+    render(todoList);
 }
 
-const render = () =>{
-    let resultHTML = todoList.map((item, i) => {
+const render = (list) =>{
+    let resultHTML = list.map((item, i) => {
         if (item.isDone == false) {
             return `<li>${item.content} <a href='#' onclick='remove(${i})'>x</a><a href='#' onclick='toggleDone(${i})'>Mark Done</a></li>\n`;
          } else {
@@ -19,10 +19,23 @@ const render = () =>{
 
 function remove (i){
     todoList.splice(i, 1)
-    render()
+    render(todoList)
 }
 
 function toggleDone (i){
     todoList[i].isDone = !(todoList[i].isDone)
-    render()
+    render(todoList)
+}
+
+const filterList = (type) => {
+    if (type=='all'){
+        render(todoList)
+    }else if (type=='done'){
+        let filteredList = todoList.filter(item => item.isDone==true);
+        render(filteredList)
+    }else if (type=='not done'){
+        let filteredList = todoList.filter(item => item.isDone==false);
+        render(filteredList)
+    }
+    
 }
